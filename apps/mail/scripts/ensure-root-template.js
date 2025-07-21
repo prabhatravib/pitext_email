68fd6df7af393e333750c5aa0fcd6ad0edd423ff
prabhatravib <prabhatravib@gmail.com>
2025-07-21 19:08:04 -0400

    changes

--- apps/mail/scripts/ensure-root-template.js
+++ apps/mail/scripts/ensure-root-template.js
@@ -1,5 +1,9 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const src = path.join(__dirname, '../index.html');
 const dest = path.join(__dirname, '../build/client/client/index.html');
