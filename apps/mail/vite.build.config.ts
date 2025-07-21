8c689ab9f2510b1072904471265b03d5b07a16ca
prabhatravib <prabhatravib@gmail.com>
2025-07-21 14:05:59 -0400

    Update vite.build.config.ts

--- apps/mail/vite.build.config.ts
+++ apps/mail/vite.build.config.ts
@@ -1,6 +1,7 @@
 import { reactRouter } from '@react-router/dev/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from 'tailwindcss';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
